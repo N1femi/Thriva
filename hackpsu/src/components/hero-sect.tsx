010e5f5edcab3a0114df0b268ad692f0b80d2c49
Jaden Torreon <jadentorreon7@gmail.com>
2025-10-25 17:11:55 -0400

    Started home page HTML

---
+++ hackpsu/src/components/hero-sect.tsx
@@ -0,0 +1,39 @@
+export const HeroSection = () => (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '40px'}}>
+       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <div>
+            <h2>Hello, USER!</h2>
+        </div>
+       <div>
+        <h2>Todays Focus:</h2>
+       </div>
+       </div>
+
+       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+       <div>
+       <p>TEXT....</p>
+       </div>
+       <div>
+       <p>TEXT....</p>
+       </div>
+       </div>
+       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <div>
+            <h2>Badges:</h2>
+        </div>
+        <div>
+            <h2>Upcoming Events</h2>
+        </div>
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <div>
+            <p>TEXT....</p>
+        </div>
+        <div>
+            <p>TEXT....</p>
+        </div>
+        </div>
+        </div>
+        
+)
